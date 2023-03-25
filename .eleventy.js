cd6eb9e150daf3ec3bae8e3133db34099d7fc700
Janeth <janeth_2124@hotmail.com>
2023-03-24 23:13:01 -0600

    bilingue

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ const {EleventyI18nPlugin} = require('@11ty/eleventy');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyI18nPlugin, {
-    defaultLanguage: 'en' // Required
+    defaultLanguage: 'es' // Required
   });
 
   eleventyConfig.addPassthroughCopy('src/favicon.ico');
